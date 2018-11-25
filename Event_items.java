f4a10a5a7e1e91913c047e3cf811626c3529a0f3
rachun085 <rachun085@users.noreply.github.com>
2018-11-25 20:34:11 +0700

    PIGFARM_2

---
+++ Event_items.java
@@ -0,0 +1,20 @@
+package com.example.admin.pigfarm.ManageData_Page;
+
+public class Event_items {
+
+    public String eventname;
+    public String event_recorddate;
+
+    public Event_items(String eventname, String event_recorddate){
+        this.eventname = eventname;
+        this.event_recorddate = event_recorddate;
+    }
+
+    public String getEventname() {
+        return eventname;
+    }
+
+    public String getEvent_recorddate() {
+        return event_recorddate;
+    }
+}
